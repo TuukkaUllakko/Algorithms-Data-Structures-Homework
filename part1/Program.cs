387541c52f3ba9b58c96429c8366b5a26ed8125b
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-05-07 19:57:11 +0300

    Exercise 1 done!

--- part1/Program.cs
+++ part1/Program.cs
@@ -6,19 +6,21 @@ namespace part1
     class Program
     {
         static void Main(string[] args)
-        {   
+        {
             //Exercise 1
-            /*Numbers num = new Numbers();
+            Numbers num = new Numbers();
             Console.WriteLine(num.Sum(4075)); // 16
             Console.WriteLine(num.Sum(3)); // 3
-            Console.WriteLine(num.Sum(999999999)); // 81*/
+            Console.WriteLine(num.Sum(999999999)); // 81
 
             //Exercise 2
+            /*DateTime start = DateTime.Now;
             Substrings subs = new Substrings();
             Console.WriteLine(subs.Calculate("aybabtu", "bab")); // 1
             Console.WriteLine(subs.Calculate("aaaaa", "aa")); // 4
             Console.WriteLine(subs.Calculate("monkey", "banana")); // 0
-
+            DateTime end = DateTime.Now;
+            Console.WriteLine("Time this took: " + end.Subtract(start));*/
 
         }
     }
--- part1/exercise1.cs
+++ part1/exercise1.cs
@@ -2,26 +2,23 @@ using System;
 using System.Collections.Generic;
 namespace part1
 {
-  public class Numbers
-  {
-    public int Sum(int x)
+    public class Numbers
     {
-      int sum = 0;
-      for (int i = 0; i < GetIntArray(x).Length; i++)
-      {
-        sum = sum + GetIntArray(x)[i];
-      }
-      return sum;
+        public int Sum(int x)
+        {
+            int sum = 0; //The sum that needs to be returned
+            List<int> listOfInts = new List<int>(); //List for storing the individual numbers
+
+            while (x > 0) //Until there are no proper numbers left
+            {
+                listOfInts.Add(x % 10); // number % 10 = it's last number. Last number gets added to the list as an individual character.
+                x = x / 10; //Basically, removes the last individual character from the number, f.ex. 48 / 10 = 4,8. It gets rounded down so -> 4.
+            }
+            for (int i = 0; i < listOfInts.Count; i++) //For each character's index in the list
+            {
+                sum = sum + listOfInts[i]; //Add the item at that index to the sum
+            }
+            return sum; //Return the answer
+        }
     }
-    int[] GetIntArray(int num)
-{
-    List<int> listOfInts = new List<int>();
-    while(num > 0)
-    {
-        listOfInts.Add(num % 10);
-        num = num / 10;
-    }
-    return listOfInts.ToArray();
-}
-  }
 }
