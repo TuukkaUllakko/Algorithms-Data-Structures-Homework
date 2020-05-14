7a30cfae4bd783b5690267ad5f34cd9efca7476d
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-05-14 14:17:00 +0300

    Exercise 2 done!

--- part2/Program.cs
+++ part2/Program.cs
@@ -9,7 +9,7 @@ namespace part2
         {
             //Exercise 1
 
-            Counters counters = new Counters();
+            /*Counters counters = new Counters();
 
             string input10 = counters.CreateInput(10);
             string input100 = counters.CreateInput(100);
@@ -25,7 +25,14 @@ namespace part2
             DateTime start2 = DateTime.Now;
             counters.EfficientCounter(input10000); //O(n)
     	    DateTime end2 = DateTime.Now;
-            Console.WriteLine("Time this took: " + end2.Subtract(start2));
+            Console.WriteLine("Time this took: " + end2.Subtract(start2));*/
+
+            //Exercise 2
+            /*LongestRepetition p = new LongestRepetition();
+
+            Console.WriteLine(p.Calculate(new int[] { 1, 2, 1, 1, 2 })); // 2
+            Console.WriteLine(p.Calculate(new int[] { 1, 2, 3, 4, 5 })); // 1
+            Console.WriteLine(p.Calculate(new int[] { 1, 1, 1, 1, 1 })); // 5*/
         }
     }
 }
---
+++ part2/exercise2.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace part2
+{
+    class LongestRepetition
+    {
+        public int Calculate(int[] t)
+        {
+            int repetition = 1;
+            int biggestRep = 1;
+
+            for (int i = 1; i < t.Length; i++)
+            {
+
+                if (t[i] == t[i - 1])
+                {
+                    repetition++;
+                }
+                else if (t[i] != t[i - 1])
+                {
+                    repetition = 1;
+                }
+                if (biggestRep < repetition)
+                {
+                    biggestRep = repetition;
+                }
+            }
+
+            return biggestRep;
+        }
+    }
+}
