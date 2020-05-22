c743b84a0ff8eabca7f1acd4503fcbd071814685
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-05-22 15:56:41 +0300

    Exercise 4 done!

--- part3/Program.cs
+++ part3/Program.cs
@@ -6,15 +6,61 @@ namespace part3
     {
         static void Main(string[] args)
         {
+            //Random number generator
+            static int[] Randomizer(int n)
+            {
+                Random random = new Random();
+                int[] arr = new int[n];
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    // integers between 1 and 1000 are enough for us
+                    arr[i] = random.Next(1, 1001);
+                }
+                return arr;
+            }
+
             /*//Exercise 1
             Exercise1 hello = new Exercise1();
             hello.Hello(5);*/
 
-            //Exercise 2
+            /*//Exercise 2
             SmallestDifference s = new SmallestDifference();
             Console.WriteLine(s.Calculate(new int[] { 4, 1, 8, 5 })); // 1
             Console.WriteLine(s.Calculate(new int[] { 1, 10, 100 })); // 9
-            Console.WriteLine(s.Calculate(new int[] { 1, 1, 1, 1, 1 })); // 0
+            Console.WriteLine(s.Calculate(new int[] { 1, 1, 1, 1, 1 })); // 0*/
+
+            /*//Exercise 3 NOT DONE!!!!!
+
+            Sorting s = new Sorting();
+            int[] sortMe = Randomizer(100);
+            int[] sortMeLarge = Randomizer(1000000);
+
+            DateTime start = DateTime.Now;
+            s.QuickSort(sortMe);
+            DateTime end = DateTime.Now;
+            Console.WriteLine("Time this took: " + end.Subtract(start));
+
+            DateTime start2 = DateTime.Now;
+            s.MergeSort(sortMe);
+            DateTime end2 = DateTime.Now;
+            Console.WriteLine("Time this took: " + end.Subtract(start));
+
+            DateTime start3 = DateTime.Now;
+            s.QuickSort(sortMeLarge);
+            DateTime end3 = DateTime.Now;
+            Console.WriteLine("Time this took: " + end.Subtract(start));
+
+            DateTime start4 = DateTime.Now;
+            s.MergeSort(sortMeLarge);
+            DateTime end4 = DateTime.Now;
+            Console.WriteLine("Time this took: " + end.Subtract(start));*/
+
+            /*//Exercise 4
+            BinarySearch b = new BinarySearch();
+            Console.WriteLine(b.Find((new int[] { 4, 1, 8, 5 }), 2)); // false
+            Console.WriteLine(b.Find((new int[] { 0, 0 }), 0)); // true
+            Console.WriteLine(b.Find((new int[] { 4, 1, 8, 5, 8, 7, 4, 2, 3 }), 2)); // true
+            Console.WriteLine(b.Find((new int[] { 0 }), 0)); // true*/
         }
     }
 }
---
+++ part3/exercise4.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace part3
+{
+    class BinarySearch
+    {
+       public bool Find(int[] t, int x)
+       {
+           Array.Sort(t);
+
+           int a = 0;
+           int b = t.Length - 1;
+
+           while (a <= b)
+           {
+               int k = (a + b) / 2;
+               
+               if (t[k] == x)
+               {
+                   return true;
+               }
+
+               if (t[k] < x)
+               {
+                   a = k + 1;
+               }
+
+               if (t[k] > x)
+               {
+                   b = k - 1;
+               }
+           }
+           return false;
+       } 
+    }
+}
