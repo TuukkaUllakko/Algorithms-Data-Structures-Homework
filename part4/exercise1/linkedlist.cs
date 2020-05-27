c3e4e5c210010cdcfab1521382930d1fd0753a1b
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-05-27 15:51:47 +0300

    Part 4 file structure

---
+++ part4/exercise1/linkedlist.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace part4
+{
+    class LinkedList
+    {
+        public void AddFirst(int n)
+        {
+
+        }
+
+        public void AddLast(int n)
+        {
+
+        }
+
+        public void RemoveFirst()
+        {
+
+        }
+
+        public void RemoveLast()
+        {
+            
+        }
+    }
+}
